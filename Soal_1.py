618e54ff1eecc05cc42edbebf71b8fd78dbae440
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2024-05-07 15:46:21 +0700

    add uts

---
+++ Soal_1.py
@@ -0,0 +1,15 @@
+def jumlah(a,b):
+    hasil_jumlah = a + b
+    return hasil_jumlah
+
+def kurang(c,d):
+    hasil_kurang = c - d
+    return hasil_kurang
+
+def mod(e,f):
+    hasil_mod = e % f
+    return hasil_mod
+
+jumlah(100,200)
+kurang(150,50)
+mod(10,2)
