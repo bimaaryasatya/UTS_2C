73b3ec712b6dcfda021c58596936905715a8085d
bimaaryasatya <145569118+bimaaryasatya@users.noreply.github.com>
2024-05-07 16:13:08 +0700

    add uts

---
+++ Soal_3.py
@@ -0,0 +1,10 @@
+def hargabarang():
+    while True:
+        jumlah_barang = int(input('Masukkan Jumlah Barang: '))
+        harga_barang = int(input('Masukkan Jumlah Barang: '))
+        Total = jumlah_barang * harga_barang
+        print(f'Jumlah Barang = {jumlah_barang}\n',f'Harga Barang = {harga_barang}\n',f'Total Belanjaan = Rp.{Total:,.2f}')
+        break
+
+hasil = hargabarang()
+print(hasil)
